e99d6853c11f3922ea115fe9f7d58c7c6fc67236
Aimen <232915@students.au.edu.pk>
2024-01-15 14:11:53 +0500

    dd

--- main.js
+++ main.js
@@ -48,3 +48,25 @@ document.addEventListener("DOMContentLoaded", function () {
 function subscribe() {
   alert('You Have Successfully Subscribed To Quillium')
 }
+
+//hamburger
+window.onload = function() {
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => link.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+};
